d47a870d431f172b31fadf99143e9e0d18a333a0
swaraj-ssk <swarajkalbande123@gmail.com>
2023-05-28 15:19:50 +0530

    Hibernate first run

---
+++ src/main/java/com/VIT/DemoHib/Alien.java
@@ -0,0 +1,33 @@
+package com.VIT.DemoHib;
+
+import javax.persistence.*;
+
+@Entity
+public class Alien {
+	
+	@Id
+	private int aid;
+	
+	private String aname;
+	private String color;
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+}
---
+++ src/main/java/com/VIT/DemoHib/App.java
@@ -0,0 +1,33 @@
+package com.VIT.DemoHib;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Alien VIT = new Alien();
+        VIT.setAid(102);
+        VIT.setAname("Pranjali");
+        VIT.setColor("Blue");
+        
+        Configuration con = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Alien.class);
+        
+        ServiceRegistry reg = new StandardServiceRegistryBuilder().applySettings(con.getProperties()).build();
+        
+        SessionFactory sf = con.buildSessionFactory(reg);
+        
+        Session session = sf.openSession();
+        
+        Transaction tx = session.beginTransaction();
+    
+        session.save(VIT);
+        
+        tx.commit();
+    }
+}
